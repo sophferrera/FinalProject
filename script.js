1e7f1b391bb7442a4a6b950761b70aa9d060e149
Sophia Ferrera <slf445@nyu.edu>
2020-12-07 01:05:36 -0500

    more content, new timing

--- script.js
+++ script.js
@@ -1,35 +1,100 @@
+var jumpTimer;
+
+let videoIDs = ["QwZT7T-TXT0", "0tpjSq9uWsI", "zI2I319ilsY", "vwLiy4ti9is", "OlUdfLBiCJ4","Su6FeI7lHVg", "ardRp2x0D_E", "2ZBsRZ6QikI", "LAJArLC6v70", "1aIYkgTcHBw", "VYm-n4BEJNI", "GDreFpd5IV4", "OX-g_MZdW9c", "pOMuZjlFTLc", "WAOkXN7aXxg", "LAJArLC6v70", "Bm6MKHO0_CI","zluEUZmECBs","91SzTz9cYTQ"];
+let playerObjects = [];
+var launchContainerElement;
+var launchButtonElement;
+var ready = false;
+
+
 window.addEventListener('DOMContentLoaded', (event) => {
 
-  //this is supposed to be disabling full screen mode, keyboard controls, and video player controls
-  for (var h = 0; h < playerObjects.length; h++){
-    playerObjects[h].disablekb();
-    playerObjects[h].fs(0);
-    playerObjects[h].controls(0);
-  }
+  console.log("DOM LOADED...");
 
-  setInterval(function() {
+  launchContainerElement = document.getElementById("launchContainer");
+  launchButtonElement = document.getElementById("launchButton");
 
-    for (var i = 0; i < playerObjects.length; i++) {
-      //trying to choose a random video from array to play for 5 or 6 seconds at a time
-      //probably means I need to nest the setInterval function inside itself?
-      // let randomVideo = playerObjects[Math.floor(Math.random) * playerObjects.length];
-      let videoLength = playerObjects[i].getDuration();
-      playerObjects[i].seekTo(Math.random() * videoLength);
-      playerObjects[i].setVolume(90);
-      playerObjects[i].unMute();
-      playerObjects[i].autoplay(1);
-      playerObjects[k].play();
+  launchButtonElement.addEventListener('click', function() {
 
-      //if the video is longer than 15 minutes it plays twice as fast
-      if (videoLength > 900) {
-        playerObjects[i].setPlaybackRate(2);
-      }
+    launchButtonElement.style.pointerEvents = 'none';
 
+    launchContainerElement.style.opacity = 0.0;
+    console.log("CLICKED!");
+
+    window.setTimeout(function() {
+      jumpTimer = setInterval(volumeSweep, 1500)
+    }, 1500);
+
+    // 2. This code loads the IFrame Player API code asynchronously.
+    // tag.src = "https://www.youtube.com/iframe_api";
+    // var firstScriptTag = document.getElementsByTagName('script')[0];
+    // firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    // 3. This function creates an <iframe> (and YouTube player)
+    //    after the API code downloads.
+
+    for (let i = 0; i < videoIDs.length; i++) {
+      // let newPlayer;
+      var newPlayerDivID = "video_" + i;
+      console.log("Generating video and placing into DIV with ID of: " + newPlayerDivID);
+      playerObjects.push(new YT.Player(newPlayerDivID, {
+        height: '390',
+        width: '640',
+        videoId: videoIDs[i],
+        playerVars: {
+          'autoplay': 0,
+          'controls': 0,
+          'disablekb': 1,
+          'fs': 0,
+          'loop': 1,
+          'modestbranding': 1,
+          'rel': 0,
+          'showinfo': 0,
+          'mute': 0,
+          'autohide': 1
+        },
+        events: {
+          'onReady': onPlayerReady,
+          'onStateChange': onPlayerStateChange
+        }
+      }));
     }
 
-  }, 2000);
+    console.log(playerObjects);
 
-
+  });
 
 
 });
+
+function volumeSweep() {
+
+  if (ready == false) {
+    return;
+  }
+
+  let videoIndexToUnmute = Math.floor(Math.random() * playerObjects.length);
+  console.log("Index to unmute = " + videoIndexToUnmute);
+
+  for (var i = 0; i < playerObjects.length; i++) {
+    if (playerObjects[i].getPlayerState() == 1) {
+      if (i == videoIndexToUnmute) {
+        if (playerObjects[i].isMuted()) {
+          console.log("UNMUTING #" + i);
+          playerObjects[i].setPlaybackRate(Math.random()*2.0); // Playback rate randomization
+          playerObjects[i].unMute(); // Mute Toggling
+          // document.getElementById("video_" + i).style.visibility = "visible"; // Visibility toggling
+        }
+      } else {
+        if (!playerObjects[i].isMuted()) {
+          console.log("Muting #" + i);
+          playerObjects[i].mute(); // Mute Toggling
+          // document.getElementById("video_" + i).style.visibility = "hidden"; // Visibility toggling
+        }
+      }
+    } else {
+      console.log("Something is wrong with player #" + i);
+    }
+  }
+
+}
